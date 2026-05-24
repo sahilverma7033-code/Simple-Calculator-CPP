8f4118db6a9bf38da040210a3d9fc055b1f6a5d9
sahilverma7033-code <sahilverma7033@gmail.com>
2026-05-24 11:18:29 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+
+ // Simple Calculator in C++
+ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare variables
+    int a = 10, b = 5;
+
+    // Perform calculations and display output
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << a / b << endl;
+
+    return 0;
+}
+
+/*
+ * Expected Output:
+ * Addition = 15
+ * Subtraction = 5
+ * Multiplication = 50
+ * Division = 2
+ *
+ * Work Hard Dream Big 🔥
+ */
